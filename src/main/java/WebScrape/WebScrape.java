530ee81d3a2c9827e106acc40d87eab5491c69b7
Jakub <jakubkrys@gmail.com>
2022-08-17 21:16:47 +0200

    Pobranie zawartości strony

--- src/main/java/WebScrape/WebScrape.java
+++ src/main/java/WebScrape/WebScrape.java
@@ -1,9 +1,20 @@
 package WebScrape;
 
+import org.jsoup.*;
+import org.jsoup.nodes.*;
+
 public class WebScrape {
 
     public static void main(String[] args) {
 
-        final String url = "https://sportalic.com/tables/england";
+        final String url = "https://sportalic.com/tip/crystal-palace-arsenal-premier-league-england-721075";
+
+        try {
+            final Document document = Jsoup.connect(url).get();
+            System.out.println(document.outerHtml());
+
+        } catch (Exception e) {
+            System.out.println(e);
+        }
     }
 }
