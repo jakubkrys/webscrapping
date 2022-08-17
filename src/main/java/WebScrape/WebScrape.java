25b26f2a2c49b7ec148e4f9f9e01500d82340a1b
Jakub <jakubkrys@gmail.com>
2022-08-17 23:15:07 +0200

    Rozwinięcie pobierania danych o dodatkowe dane

--- src/main/java/WebScrape/WebScrape.java
+++ src/main/java/WebScrape/WebScrape.java
@@ -12,16 +12,21 @@ public class WebScrape {
 
         try {
             Document document = Jsoup.connect(url).get();
-            Element content = document.getElementById("game-details-wrapper");
+            Element content = document.getElementById("game-details-wrapper"); 
+            Elements gameTime = content.getElementsByClass("game-time");
+            Elements homeTeam = content.getElementsByClass("team-flag-left");
+            Elements awayTeam = content.getElementsByClass("team-flag-right");
             Elements finalResult = content.getElementsByClass("game-result");
             Elements result1H2H = content.getElementsByClass("game-extended-result");
+            Elements homeTeamWinnerBet = content.getElementsByClass("game-stat-wrapper");
+//            Elements drawBet = content.getElementsByClass("odd down ");
+//            Elements awayTeamWinnerBet = content.getElementsByClass("odd up ");
 
+            System.out.println(gameTime.text().substring(0, (gameTime.text().length() - 6)));
+            System.out.println(homeTeam.text()+ " - "+awayTeam.text());
             System.out.println(finalResult.text()+" "+result1H2H.text());
-//            for (Element link : links) {
-//                String linkText = link.text();
-//                System.out.println(linkText);
-//            }
-            //System.out.println(document.outerHtml());
+//            System.out.println(homeTeamWinnerBet.text()+" ; "+drawBet.text()+" ; "+awayTeamWinnerBet.text());
+            System.out.println(homeTeamWinnerBet.text());
 
         } catch (Exception e) {
             System.out.println(e);
